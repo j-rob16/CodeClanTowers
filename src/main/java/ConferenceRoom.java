e39be25996c49903cae215b0d62e82d5dd3e9239
James R <jimmer1609@gmail.com>
2022-08-05 19:20:59 +0900

    added conference

--- src/main/java/ConferenceRoom.java
+++ src/main/java/ConferenceRoom.java
@@ -1,2 +1,12 @@
-public class ConferenceRoom {
+public class ConferenceRoom extends Room {
+    private String name;
+
+    public ConferenceRoom(int capacity, String name) {
+        super(capacity);
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
 }
--- src/test/java/ConferenceRoomTest.java
+++ src/test/java/ConferenceRoomTest.java
@@ -1,2 +1,23 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ConferenceRoomTest {
+    ConferenceRoom conferenceRoom;
+
+    @Before
+    public void before() {
+        conferenceRoom = new ConferenceRoom(25, "The Conference Room");
+    }
+
+    @Test
+    public void canGetRoomName() {
+        assertEquals("The Conference Room", conferenceRoom.getName());
+    }
+
+    @Test
+    public void canGetCapacity() {
+        assertEquals(25, conferenceRoom.getCapacity());
+    }
 }
